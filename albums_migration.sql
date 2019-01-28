8c8aebc53e3d1de3bd9a9534a0277c0bab87a71f
rob-moore1894 <moore.re@gmail.com>
2019-01-28 13:36:56 -0600

    Started MySQL exercises

---
+++ albums_migration.sql
@@ -0,0 +1,22 @@
+USE codeup_test_db;
+
+drop table if exists albums;
+
+-- Create an albums table with the following columns:
+
+-- id — auto incrementing unsigned integer primary key
+-- artist — string for storing the recording artist name
+-- name — string for storing a record name
+-- release_date — integer representing year record was released
+-- sales — floating point value for number of records sold (in millions)
+-- genre — string for storing the record's genre(s)
+
+CREATE TABLE albums (
+    id INT UNSIGNED AUTO_INCREMENT,
+    artist VARCHAR(50),
+    name  VARCHAR(100),
+    release_date INT,
+    sales double,
+    genre VARCHAR(50)
+    PRIMARY KEY (id)
+);
