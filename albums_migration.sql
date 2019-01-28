95d834f58a64b9a10647ae891127dd8ff723a57e
rob-moore1894 <moore.re@gmail.com>
2019-01-28 14:14:10 -0600

    Fixed the syntax error in the script

--- albums_migration.sql
+++ albums_migration.sql
@@ -11,12 +11,12 @@ drop table if exists albums;
 -- sales — floating point value for number of records sold (in millions)
 -- genre — string for storing the record's genre(s)
 
-CREATE TABLE albums (
-    id INT UNSIGNED AUTO_INCREMENT,
+CREATE TABLE if not exists albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50),
     name  VARCHAR(100),
-    release_date INT,
+    release_date DATE,
     sales double,
-    genre VARCHAR(50)
+    genre VARCHAR(50),
     PRIMARY KEY (id)
 );
